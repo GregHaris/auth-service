89ad38b498d9ad0f190f016cd10dfdad68f8a35e
Greg Haris <d.gregharis@gmail.com>
2026-04-13 14:12:25 +0100

    feat: initialize the backend and frontend

---
+++ apps/web/next.config.ts
@@ -0,0 +1,18 @@
+// import type { NextConfig } from "next";
+
+// const nextConfig: NextConfig = {
+//   /* config options here */
+// };
+
+// export default nextConfig;
+
+import type { NextConfig } from 'next';
+import path from 'path';
+
+const nextConfig: NextConfig = {
+  turbopack: {
+    root: path.resolve(__dirname, '../../'),
+  },
+};
+
+export default nextConfig;
